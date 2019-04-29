eed309f35d454b3b2cb1fec7a54020bf344d3a68
Reza Esmaili <github@dfox.info>
2019-04-29 16:50:58 +0200

    added model metadata; update to laravel 5.8

--- src/Models/File.php
+++ src/Models/File.php
@@ -15,6 +15,46 @@ use Spatie\MediaLibrary\Models\Media;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
+/**
+ * Motor\Media\Models\File
+ *
+ * @property int $id
+ * @property int|null $client_id
+ * @property string $description
+ * @property string $author
+ * @property string $source
+ * @property string $alt_text
+ * @property int $is_global
+ * @property int $created_by
+ * @property int $updated_by
+ * @property int|null $deleted_by
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Kalnoy\Nestedset\Collection|\Motor\Backend\Models\Category[] $categories
+ * @property-read \Motor\Backend\Models\User $creator
+ * @property-read \Motor\Backend\Models\User|null $eraser
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\MediaLibrary\Models\Media[] $media
+ * @property-read \Motor\Backend\Models\User $updater
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File filteredBy(\Motor\Core\Filter\Filter $filter, $column)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File filteredByMultiple(\Motor\Core\Filter\Filter $filter)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File search($q, $full_text = false)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereAltText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereAuthor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereClientId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereDeletedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereIsGlobal($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereSource($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\File whereUpdatedBy($value)
+ * @mixin \Eloquent
+ */
 class File extends Model implements HasMedia
 {
 
--- src/Models/FileAssociation.php
+++ src/Models/FileAssociation.php
@@ -4,6 +4,32 @@ namespace Motor\Media\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Motor\Media\Models\FileAssociation
+ *
+ * @property int $id
+ * @property int $file_id
+ * @property string $model_type
+ * @property int $model_id
+ * @property string $identifier
+ * @property string $custom_properties
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Motor\Media\Models\File $file
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $model
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation whereCustomProperties($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation whereFileId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation whereIdentifier($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation whereModelId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation whereModelType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Motor\Media\Models\FileAssociation whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class FileAssociation extends Model
 {
 
