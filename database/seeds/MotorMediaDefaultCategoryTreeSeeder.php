31d7797d687f486de53070f1deff3873cd36885b
Reza Esmaili <bitbucket@dfox.info>
2019-11-19 18:00:51 +0100

    Add seeders

---
+++ database/seeds/MotorMediaDefaultCategoryTreeSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Class MotorMediaDefaultCategorySeeder
+ */
+class MotorMediaDefaultCategoryTreeSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $node = \Motor\Backend\Models\Category::create([
+            'name'       => 'Media',
+            'scope'      => 'media',
+            'created_by' => 1,
+            'updated_by' => 1,
+
+            'children' => [
+                [
+                    'name'       => 'Images',
+                    'scope'      => 'media',
+                    'created_by' => 1,
+                    'updated_by' => 1,
+                ],
+                [
+                    'name'       => 'Videos',
+                    'scope'      => 'media',
+                    'created_by' => 1,
+                    'updated_by' => 1,
+                ],
+                [
+                    'name'       => 'Documents',
+                    'scope'      => 'media',
+                    'created_by' => 1,
+                    'updated_by' => 1,
+                ],
+            ],
+        ]);
+    }
+}
