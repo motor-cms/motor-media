dbbaf106f5c989924d7fb09560409013d549e175
Reza Esmaili <bitbucket@dfox.info>
2022-07-01 18:28:23 +0200

    Fix api docs

--- src/Http/Requests/Backend/FilePatchRequest.php
+++ src/Http/Requests/Backend/FilePatchRequest.php
@@ -6,61 +6,60 @@ use Motor\Admin\Http\Requests\Request;
 
 /**
  * Class FilePatchRequest
+ *
+ * @OA\Schema(
+ *   schema="FilePatchRequest",
+ *   @OA\Property(
+ *     property="client_id",
+ *     type="integer",
+ *     example="1"
+ *   ),
+ *   @OA\Property(
+ *     property="description",
+ *     type="string",
+ *     example="Exhaustive description of the file"
+ *   ),
+ *   @OA\Property(
+ *     property="author",
+ *     type="string",
+ *     example="Reza Esmaili"
+ *   ),
+ *   @OA\Property(
+ *     property="source",
+ *     type="string",
+ *     example="Some photographer"
+ *   ),
+ *   @OA\Property(
+ *     property="alt_text",
+ *     type="string",
+ *     example="Alternative Text For The IMG Tag"
+ *   ),
+ *   @OA\Property(
+ *     property="is_global",
+ *     type="boolean",
+ *     example="true"
+ *   ),
+ *   @OA\Property(
+ *     property="categories",
+ *     type="array",
+ *     description="Array of category ids",
+ *     @OA\Items(
+ *       anyOf={
+ *         @OA\Schema(type="integer")
+ *       }
+ *     ),
+ *     example="[1,3]"
+ *   ),
+ *   @OA\Property(
+ *     property="file",
+ *     type="object",
+ *     ref="#/components/schemas/FileUpload",
+ *     description="If false, the existing file will be deleted"
+ *   ),
+ * )
  */
 class FilePatchRequest extends Request
 {
-    /**
-     * @OA\Schema(
-     *   schema="FilePatchRequest",
-     *   @OA\Property(
-     *     property="client_id",
-     *     type="integer",
-     *     example="1"
-     *   ),
-     *   @OA\Property(
-     *     property="description",
-     *     type="string",
-     *     example="Exhaustive description of the file"
-     *   ),
-     *   @OA\Property(
-     *     property="author",
-     *     type="string",
-     *     example="Reza Esmaili"
-     *   ),
-     *   @OA\Property(
-     *     property="source",
-     *     type="string",
-     *     example="Some photographer"
-     *   ),
-     *   @OA\Property(
-     *     property="alt_text",
-     *     type="string",
-     *     example="Alternative Text For The IMG Tag"
-     *   ),
-     *   @OA\Property(
-     *     property="is_global",
-     *     type="boolean",
-     *     example="true"
-     *   ),
-     *   @OA\Property(
-     *     property="categories",
-     *     type="array",
-     *     description="Array of category ids",
-     *     @OA\Items(
-     *       anyOf={
-     *         @OA\Schema(type="integer")
-     *       }
-     *     ),
-     *     example="[1,3]"
-     *   ),
-     *   @OA\Property(
-     *     property="file",
-     *     type="object",
-     *     ref="#/components/schemas/FileUpload",
-     *     description="If false, the existing file will be deleted"
-     *   ),
-     * )
-     */
 
     /**
      * Determine if the user is authorized to make this request.
