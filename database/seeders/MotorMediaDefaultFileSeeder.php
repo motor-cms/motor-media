c3da4e613a9a44696272aaa93ce74479e4b9d445
Reza Esmaili <bitbucket@dfox.info>
2023-04-28 16:19:25 +0200

    Fixed seedes

--- database/seeders/MotorMediaDefaultFileSeeder.php
+++ database/seeders/MotorMediaDefaultFileSeeder.php
@@ -5,9 +5,7 @@ namespace Motor\Media\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-use Motor\Admin\Http\Resources\MediaResource;
 use Motor\Media\Models\File;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Storage;
 
 /**
@@ -32,24 +30,23 @@ class MotorMediaDefaultFileSeeder extends Seeder
         $imageFile = file_get_contents('https://images.pexels.com/photos/7336640/pexels-photo-7336640.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
 
         foreach ($files as $file) {
-            $filename = Str::random(10).".png";
-            Storage::disk('media')->put( $file->id . '/' . $filename, $imageFile);
-            sleep(1);
+            $filename = Str::random(10).'.png';
+            Storage::disk('media')->put($file->id.'/'.$filename, $imageFile);
             $fileSize = strlen($imageFile);
-            DB::table("media")->insert([
+            DB::table('media')->insert([
                 'model_type' => "Motor\Media\Models\File",
                 'model_id' => $file->id,
-                'collection_name' => "file",
+                'collection_name' => 'file',
                 'name' => $filename,
                 'file_name' => $filename,
-                'mime_type' => "image/png",
-                'disk' => "media",
+                'mime_type' => 'image/png',
+                'disk' => 'media',
                 'size' => $fileSize,
                 'manipulations' => '[]',
                 'custom_properties' => '[]',
                 'responsive_images' => '[]',
                 'order_column' => 1,
-                'conversions_disk' => "media",
+                'conversions_disk' => 'media',
                 'uuid' => Str::uuid(),
                 'generated_conversions' => '{"thumb": true, "preview": true}',
             ]);
