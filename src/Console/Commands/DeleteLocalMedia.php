6e715642ac272a5c6d4f493ff8dcc0876551bb81
Florian Russy <fr@zeitraum.com>
2024-07-24 11:33:50 +0200

    add commands to migrate media files to s3

---
+++ src/Console/Commands/DeleteLocalMedia.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Motor\Media\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Motor\Media\Models\File;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class DeleteLocalMedia extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'motor-media:delete_local_media';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        File::all()->each(function ($model) {
+            $mediaItems = $model->getMedia('file', function(Media $media) {
+                return $media->disk == 'media';
+            });
+            foreach ($mediaItems as $mediaItem) {
+                $name = $mediaItem->name;
+                $mediaItem->forceDelete();
+                $this->info("Deleted ${name}");
+            }
+        });
+        $this->info("Deletion completed");
+    }
+}
---
+++ src/Console/Commands/MigrateMedia.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Motor\Media\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Motor\Media\Models\File;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class MigrateMedia extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'motor-media:migrate_media_to_s3';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        File::all()->each(function ($model) {
+            $mediaItems = $model->getMedia('file', function(Media $media) {
+                return $media->disk == 'media';
+            });
+            foreach ($mediaItems as $mediaItem) {
+                try {
+                    $movedItem = $mediaItem->move($model, 'file', 'media-s3');
+                    $name = $movedItem->name;
+                    $this->info("Migrated ${name}");
+                } catch (Exception $e) {}
+            }
+        });
+        $this->info("Disk migration completed.");
+    }
+}
