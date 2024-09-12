ce8a8e4ed304e90177da4fdf6b8767eaccbc3ff1
Florian Haupenthal <florian@hakigo.de>
2024-09-12 14:54:12 +0200

    EN-31: Correctly merge configurations

--- src/Providers/MotorServiceProvider.php
+++ src/Providers/MotorServiceProvider.php
@@ -42,10 +42,10 @@ class MotorServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../../config/motor-media.php', 'motor-media');
+        $this->mergeConfigFrom(__DIR__ . '/../../config/motor-media.php', 'motor-media');
 
         $config = $this->app['config']->get('scout', []);
-        $this->app['config']->set('scout', array_replace_recursive(require __DIR__.'/../../config/scout.php', $config));
+        $this->app['config']->set('scout', array_merge_recursive(require __DIR__ . '/../../config/scout.php', $config));
     }
 
     /**
@@ -53,7 +53,7 @@ class MotorServiceProvider extends ServiceProvider
      */
     public function migrations()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
     }
 
     /**
@@ -62,7 +62,7 @@ class MotorServiceProvider extends ServiceProvider
     public function permissions()
     {
         $config = $this->app['config']->get('motor-admin-permissions', []);
-        $this->app['config']->set('motor-admin-permissions', array_replace_recursive(require __DIR__.'/../../config/motor-admin-permissions.php', $config));
+        $this->app['config']->set('motor-admin-permissions', array_merge_recursive(require __DIR__ . '/../../config/motor-admin-permissions.php', $config));
     }
 
     /**
@@ -71,8 +71,8 @@ class MotorServiceProvider extends ServiceProvider
     public function routes()
     {
         if (! $this->app->routesAreCached()) {
-            require __DIR__.'/../../routes/api.php';
-            require __DIR__.'/../../routes/web.php';
+            require __DIR__ . '/../../routes/api.php';
+            require __DIR__ . '/../../routes/web.php';
         }
     }
 
@@ -92,6 +92,6 @@ class MotorServiceProvider extends ServiceProvider
     public function navigationItems()
     {
         $config = $this->app['config']->get('motor-admin-navigation', []);
-        $this->app['config']->set('motor-admin-navigation', array_replace_recursive(require __DIR__.'/../../config/motor-admin-navigation.php', $config));
+        $this->app['config']->set('motor-admin-navigation', array_merge_recursive(require __DIR__ . '/../../config/motor-admin-navigation.php', $config));
     }
 }
