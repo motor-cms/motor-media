71d04b2ef96df3ce451c4783f2371f3d7e5d6ce6
Reza Esmaili <github@dfox.info>
2026-03-09 15:22:50 +0100

    feat: add V2 request classes separate from V1
    Create V2-specific Form Request classes that extend V1 counterparts,
    allowing V2 API validation to diverge independently. Update all V2
    controllers to import from V2 request namespace.
    
    Part of EN-1812: API v2 Refactoring

---
+++ src/Http/Requests/Backend/V2/FilePostRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Motor\Media\Http\Requests\Backend\V2;
+
+use Motor\Media\Http\Requests\Backend\FilePostRequest as V1FilePostRequest;
+
+class FilePostRequest extends V1FilePostRequest
+{
+    //
+}
